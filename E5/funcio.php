e65ed5e0d19fbf81f493f2e77197344909c614ec
Riu <josemanuelriu@gmail.com>
2025-11-29 11:19:47 +0100

    Exercise 5

---
+++ E5/funcio.php
@@ -0,0 +1,24 @@
+<?php
+	
+	$nota = $_POST["nota"];		
+	$boleanNota = is_numeric($nota);	
+
+	if ($nota == '' or !$boleanNota or $nota>100 or $nota<=0) {
+		echo "Error, ha d'introduir un valor numéric del 1 al 100.";
+	} 
+ 	else {
+		if ($nota>=60) {
+			echo "El grau es de Primera Divisió";
+		}
+		elseif ($nota>=45) {
+			echo "El grau es de Segona Divisió";
+		}
+		elseif ($nota>=33) {
+			echo "El grau es de Tercera Divisió";
+		}
+		else {
+			echo "L'estudiant reprovarà";
+		}
+	}
+
+?>
